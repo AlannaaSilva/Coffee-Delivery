f11da18bde6316e2ebed05c2d580e629668e3564
AlannaaSilva <alanna.silva@ilegra.com>
2024-11-19 10:10:17 -0300

    update styles

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route} from 'react-router-dom';
-import { HomePage } from './pages/Home';
-import { CompletedOrder } from './pages/CompletedOrder';
-import { DefaultLayout } from './layouts/DefaultLayout';
-import { OrderConfirmedPage } from './pages/OrderConfirmed';
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/Home";
+import { CompletedOrder } from "./pages/CompletedOrder";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+import { OrderConfirmedPage } from "./pages/OrderConfirmed";
 
-export function Router(){
-  return(
+export function Router() {
+  return (
     <Routes>
-      <Route path = "/" element = {<DefaultLayout/>}>
-        <Route path = "/" element={<HomePage/>}/>
-        <Route path = "/completedOrder" element={<CompletedOrder/>}/>
-        <Route path="/orderConfirmed" element={<OrderConfirmedPage/>}/>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/completedOrder" element={<CompletedOrder />} />
+        <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
+        <Route path="/category/cafes" element={<HomePage />} />
+        <Route path="/category/acessorios" element={<HomePage />} />
+        <Route path="/category/complementos" element={<HomePage />} />
       </Route>
-
     </Routes>
-  )
+  );
 }
